31b45293c32df5a390c76636a9b779cff08ebc95
rankspitter <65015162@gmail.com>
2023-09-07 23:00:22 +0700

    learn struct with aespa

--- aespa.c
+++ aespa.c
@@ -1,33 +1,31 @@
+#include <stdio.h>
+#include <string.h>
 
-#include<stdio.h>
-#include<string.h>
-
-
-typedef struct Person
-{
-        char name[20];
+typedef struct Person {
+    char name[20];
+    char position[20];
+    int age;
 } person;
 
-typedef struct Group
-{
-        person comp;
-        int phone;
-	char alias[20];
+typedef struct Group {
+    person comp;
+    char alias[20];
 } group;
 
-
-
-int main()
-{
-        group p1;
-
-        strcpy(p1.comp.name, "Winter");
-	strcpt(p1.alias, "armamentor");
-
-        printf("Hi %s!! you are frist here", p1.comp.name);
-
-
-
-        return 0;
+void printPersonInfo(group p) {
+    printf("%s\n%s\nposition: %s\nage: %d\n\n", p.comp.name, p.alias, p.comp.position, p.comp.age);
 }
 
+int main() {
+    group p1 = {{"Karina", "Main Dancer", 23}, "rocket puncher !!"};
+    group p2 = {{"Winter", "Lead Vocalist", 22}, "armamentor !!"};
+    group p3 = {{"Gisell", "Main Rapper", 22}, "got xenogloss !!"};
+    group p4 = {{"Ningning", "Main Vocals", 20}, "ed hacker !!"};
+
+    printPersonInfo(p1);
+    printPersonInfo(p2);
+    printPersonInfo(p3);
+    printPersonInfo(p4);
+
+    return 0;
+}
